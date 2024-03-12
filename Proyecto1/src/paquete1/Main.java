604979a051776a227db9a3021acaf49ee0fce3a5
missn <missn@DESKTOP-FS742R7>
2024-03-12 15:38:33 +0100

    v1

---
+++ Proyecto1/src/paquete1/Main.java
@@ -0,0 +1,10 @@
+package paquete1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("Probando repositorios Git en 1ºDAM");
+		System.out.print("Adiossss");
+	}
+
+}
